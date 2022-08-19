67280eacec989a540e637bb52ee8e5371e7ac480
--local <--local>
2022-08-19 18:53:23 -0300

    change font-family

--- api/src/main.ts
+++ api/src/main.ts
@@ -11,13 +11,6 @@ async function bootstrap() {
     origin: '*',
   });
 
-  /* app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  }); */
-
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, //Transform all requests into dto
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -7,13 +7,9 @@ export default function Document() {
     return (
         <Html lang='es' className='scroll-smooth'>
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Acme&family=Averia+Sans+Libre:wght@300&family=Kalam&family=Mochiy+Pop+One&display=swap"
-                    rel="stylesheet"
-                />
+                
             </Head>
-            <body className='bg-blackPrimary font-[Acme] dark:bg-gray-300'>
+            <body className='bg-blackPrimary font-Acme dark:bg-gray-300'>
                 <Main />
                 <NextScript />
             </body>
