585a991dd15a05cc64777b90ca9675659c7ad1e5
--local <--local>
2023-01-10 14:30:08 -0300

    change heroku to railway

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -28,11 +28,11 @@ import 'dotenv/config';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME || 'portfolio',
+      host: process.env.PGHOST,
+      port: Number(process.env.PGPORT) || 5432,
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE || 'portfolio',
       synchronize: true,
       logging: false,
       ssl: {
