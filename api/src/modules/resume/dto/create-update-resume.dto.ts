5ad39271cd1867eef344a9d5ec3ae52139f5b7cd
Angel Guillermo Montania <AngelGuillermoMontania@gmail.com>
2024-02-24 15:40:11 -0300

    delete backend

--- api/src/modules/resume/dto/create-update-resume.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreateUpdateResumeDto {
-  @IsString()
-  readonly spanish: string;
-
-  @IsString()
-  readonly english: string;
-}
--- api/src/modules/soft/dto/create-update-soft.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class CreateUpdateSoftDto {
-  @IsString()
-  name: string;
-
-  @IsString()
-  image: string;
-}
